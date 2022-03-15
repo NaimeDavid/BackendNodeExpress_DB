25b0fecaff8069a180ffb35da8cd4f6686ccf19a
Naime David <naimedavid@gmail.com>
2022-03-15 16:54:07 -0300

    Lista de heróis criada

--- index.js
+++ index.js
@@ -10,108 +10,25 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-/*LISTA DE ENDPOINTS DA APLICAÇÃO CRUD DE MENSAGENS
-  
-  [GET] - na rota /mensagens - retona a lista de mnsg
-  [GET] - na rota /mensagens/{id} - pra retornar apenas uma mensagem
-  [POST] - criar nova mensagem
-  [PUT] /mensagens - pra atualizar mgn pelo ID dela
-  [DELETE] - /mensagens/{id} - deletar mensagens pelo ID
-
-*/
-
-const mensagens = [{
+const herois = [{
         "id": 0,
-        "texto": "Essa é a primeira mensagem",
+        "heroi": "Thor"
     },
     {
         "id": 1,
-        "texto": "Segunda mensagem",
+        "heroi": "Diana"
 
-    },
-    {
+    },{
         "id": 2,
-        "texto": "Terceira mensagem",
+        "heroi": "Peter Parker"
+    },{
+        "id": 3,
+        "heroi": "Chapolin Colorado"
     }
 ]
 
-const getMensagemValida = () => {
-     return mensagens.filter(Boolean)
-};
-
-const getMensagemByID = id => {
-    return getMensagemValida().find(msg => msg.id === id)
-};
-
-//[GET] - na rota / mensagens - retona a lista de mnsg
-app.get('/mensagens', (req, res) => {
-    res.send(getMensagemValida())
-})
-
-//[GET] - na rota /mensagens/{id} - pra retornar apenas uma mensagem
-app.get('/mensagens/:id', (req, res) => {
-
-    const id = +req.params.id;
-    const mensagem = getMensagemByID(id)
-
-    if (!mensagem) {
-        res.send("Mensagem não encontrada.");
-        return;
-    }
-    res.send(mensagem)
-
-})
-
-//[POST] - paracriar nova mensagem
-app.post('/mensagens', (req, res) => {
-    const mensagem = req.body;
-
-    if (!mensagem || !mensagem.texto) {
-        res.send('Mensagem inválida')
-        return
-    }
-    mensagem.id = mensagens.length
-    mensagens.push(mensagem)
-
-    res.send(mensagem)
-
-})
-
-//[PUT] /mensagens/{id} - atualizar mgn pelo ID dela
-app.put('/mensagens/:id', (req, res) => {
-
-    const id = +req.params.id;
-    const mensagem = getMensagemByID(id)
-
-    const novoTexto = req.body.texto;
-
-    if (!novoTexto) {
-        res.send('Mensagem inválida')
-        return
-    }
-    mensagem.texto = novoTexto;
-
-    res.send(mensagem);
-
-})
-
-//[DELETE] - /mensagens/{id} - deletar mensagens pelo ID
-app.delete('/mensagens/:id', (req, res) => {
-
-    const id = +req.params.id;
-    const mensagem = getMensagemByID(id)
-
-    if(!mensagem){
-        res.send('Mensagem não encontrada')
-        return
-    }
-
-    const index = mensagens.indexOf(mensagem)
-   
-    delete mensagens[index]
-
-    res.send(`Mensagem deletada.`)
-
+app.get('/herois', function (req, res) {
+    res.send(herois)
 })
 
 
