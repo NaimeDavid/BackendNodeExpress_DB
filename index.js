9066ab8e031d4db40fc2ed7e7d7c7eb95ccec8d8
Naime David <naimedavid@gmail.com>
2022-03-15 17:11:27 -0300

    Prettier adicionado

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-const express = require('express');
-var bodyParser = require('body-parser')
+const express = require("express");
+var bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-const herois = [{
-        "id": 0,
-        "heroi": "Thor"
-    },
-    {
-        "id": 1,
-        "heroi": "Diana"
+const herois = [
+  {
+    id: 0,
+    heroi: "Thor",
+  },
+  {
+    id: 1,
+    heroi: "Diana",
+  },
+  {
+    id: 2,
+    heroi: "Peter Parker",
+  },
+  {
+    id: 3,
+    heroi: "Chapolin Colorado",
+  },
+];
 
-    },{
-        "id": 2,
-        "heroi": "Peter Parker"
-    },{
-        "id": 3,
-        "heroi": "Chapolin Colorado"
-    }
-]
-
-app.get('/herois', function (req, res) {
-    res.send(herois)
-})
-
+app.get("/herois", function (req, res) {
+  res.send(herois);
+});
 
 app.listen(port, function () {
-    console.log(`App rodando em http://localhost:${port}`)
-})
+  console.log(`App rodando em http://localhost:${port}`);
+});
